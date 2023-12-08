5ac1c4d8583a19c80cd3018c32f75b382ef90d4f
Huaqiwill <3173484026@qq.com>
2023-12-08 13:04:05 +0800

    方法换闭包

--- route/report.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use think\facade\Route;
-
-//报表操作
-Route::group("report", function () {
-    Route::get('index', 'Report/index');
-    Route::post('userAchievement', 'Report/userAchievement');
-});
